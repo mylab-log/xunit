2de95a5db2d082a997bad2429d583f8e6eb816b1
Andrey O. <ozzy.ext@gmail.com>
2023-10-18 00:20:36 +0300

    Improve tests

---
+++ src/UnitTests/Demo.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyLab.Log.XUnit;
+using Xunit.Abstractions;
+using Xunit;
+using Microsoft.Extensions.Logging.Console;
+
+namespace UnitTests
+{
+
+    public class Demo
+    {
+        private ITestOutputHelper _output;
+
+        public Demo(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void DemoOutput()
+        {
+            var appServices = new ServiceCollection()
+                .AddLogging(l => 
+                        l.AddXUnit(_output)
+                    )
+                .BuildServiceProvider();
+
+            var logger = appServices.GetRequiredService<ILogger<Demo>>();
+
+            logger.LogInformation("Foo");
+
+            Assert.True(false); //to see a Log
+        }
+
+        [Fact]
+        public void DemoFormatting()
+        {
+            var appServices = new ServiceCollection()
+                .AddLogging(l => l.AddXUnit(_output))
+                .Configure<ConsoleLoggerOptions>(o => o.FormatterName = ConsoleFormatterNames.Systemd)
+                .BuildServiceProvider();
+
+            var logger = appServices.GetRequiredService<ILogger<Demo>>();
+
+            logger.LogInformation("Foo");
+
+            Assert.True(false); //to see a Log
+        }
+    }
+}
--- src/UnitTests/LoggerBehavior.cs
+++ src/UnitTests/LoggerBehavior.cs
@@ -1,34 +1,64 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Moq;
 using MyLab.Log.XUnit;
 using Xunit.Abstractions;
+using Xunit;
 
 namespace UnitTests
 {
     public class LoggerBehavior
     {
-        private readonly ITestOutputHelper _output;
-
-        public LoggerBehavior(ITestOutputHelper output)
-        {
-            _output = output;
-        }
-
         [Fact]
         public void ShouldWriteLogToOutput()
         {
             //Arrange
+            string? outputMessage = null;
+
+            var outputMock = new Mock<ITestOutputHelper>();
+            outputMock
+                .Setup(o => o.WriteLine(It.IsAny<string>()))
+                .Callback<string>(msg => outputMessage = msg);
+
             var srv = new ServiceCollection()
-                .AddLogging(l => l.AddXUnit(_output))
+                .AddLogging(l => l.AddXUnit(outputMock.Object))
                 .BuildServiceProvider();
 
             var logger = srv.GetRequiredService<ILogger<LoggerBehavior>>();
 
             //Act
-            logger.LogInformation("Test log");
+            logger.LogInformation("Foo");
 
             //Assert
+            Assert.StartsWith($"[40m[32minfo[39m[22m[49m: UnitTests.{nameof(LoggerBehavior)}[0]", outputMessage);
+            Assert.Contains("Foo", outputMessage);
+        }
 
+        [Fact]
+        public void ShouldUseConsoleFormatter()
+        {
+            //Arrange
+            string? outputMessage = null;
+
+            var outputMock = new Mock<ITestOutputHelper>();
+            outputMock
+                .Setup(o => o.WriteLine(It.IsAny<string>()))
+                .Callback<string>(msg => outputMessage = msg);
+
+            var srv = new ServiceCollection()
+                .AddLogging(l => l.AddXUnit(outputMock.Object))
+                .Configure<ConsoleLoggerOptions>(o => o.FormatterName = ConsoleFormatterNames.Systemd)
+                .BuildServiceProvider();
+
+            var logger = srv.GetRequiredService<ILogger<LoggerBehavior>>();
+
+            //Act
+            logger.LogInformation("Foo");
+
+            //Assert
+            Assert.NotNull(outputMessage);
+            Assert.Equal($"<6>UnitTests.{nameof(LoggerBehavior)}[0] Foo", outputMessage!.TrimEnd());
         }
     }
 }
